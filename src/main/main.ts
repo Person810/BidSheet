b8ecd612152838bf14fdc61a92cbd1b969c96016
Person810 <lm.wiley@yahoo.com>
2026-03-25 17:54:23 -0500

    Add VS Code launch config and fix devtools auto-open

--- src/main/main.ts
+++ src/main/main.ts
@@ -25,7 +25,6 @@ function createWindow(): void {
 
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173');
-    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
   }
